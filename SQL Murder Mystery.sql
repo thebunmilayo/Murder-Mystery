b3620013a1b88fb0b6bd443c1bd99a4761e97ad9
thebunmilayo <141869697+thebunmilayo@users.noreply.github.com>
2023-08-13 14:12:58 +0100

    Update SQL Murder Mystery.sql

--- SQL Murder Mystery.sql
+++ SQL Murder Mystery.sql
@@ -3,6 +3,14 @@
 -- The following codes show how the answer was arrived at.
 
 
+--/ Here's a brief overview of the project:
+
+-- "A crime has taken place and the detective needs your help. The detective gave you the crime scene report, but you somehow lost it.
+-- You vaguely remember that the crime was a murder that occurred sometime on Jan.15, 2018 and that it took place in SQL City.
+-- Start by retrieving the corresponding crime scene report from the police department’s database. Figure out who committed the crime with
+-- the details you remembered above."
+
+
 select * from crime_scene_report
 where date = '20180115' and type is 'murder' and city is 'SQL City'
 
